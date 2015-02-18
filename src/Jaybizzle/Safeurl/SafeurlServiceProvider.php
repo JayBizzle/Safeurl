65ac89d96e3fc5928877c0cd172ff96da361d7ae
Mark Beech <mbeech@mark-beech.co.uk>
2015-02-18 22:34:46 +0000

    Access package config L5 style

--- src/Jaybizzle/Safeurl/SafeurlServiceProvider.php
+++ src/Jaybizzle/Safeurl/SafeurlServiceProvider.php
@@ -18,7 +18,7 @@ class SafeurlServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		//
+		$this->registerConfig();
 	}
 
 	/**
@@ -42,6 +42,33 @@ class SafeurlServiceProvider extends ServiceProvider {
 	public function provides()
 	{
 		return array();
+	}
+
+	/**
+	 * Register our config file
+	 *
+	 * @return void
+	 */
+	protected function registerConfig()
+	{
+		// The path to the user config file
+		$userConfigPath    = app()->configPath() . '/packages/jaybizzle/safeurl/config.php';
+
+		// Path to the default config
+		$defaultConfigPath = __DIR__ . '/../../config/config.php';
+
+		// Load the default config
+		$config = $this->app['files']->getRequire($defaultConfigPath);
+
+		if (file_exists($userConfigPath)) 
+		{       
+			// User has their own config, let's merge them properly
+			$userConfig = $this->app['files']->getRequire($userConfigPath);
+			$config     = array_replace_recursive($config, $userConfig);
+		}
+
+		// Set each of the items like ->package() previously did
+		$this->app['config']->set('safeurl::config', $config);
 	}
 
 }
