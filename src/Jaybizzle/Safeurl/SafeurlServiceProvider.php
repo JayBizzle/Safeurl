a0b71f98c81c9dfdf26f4e61267c967b81f1a011
Mark Beech <mbeech@mark-beech.co.uk>
2015-02-18 22:43:38 +0000

    Better config loading

--- src/Jaybizzle/Safeurl/SafeurlServiceProvider.php
+++ src/Jaybizzle/Safeurl/SafeurlServiceProvider.php
@@ -18,7 +18,11 @@ class SafeurlServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		$this->registerConfig();
+		$this->publishes([
+			__DIR__.'/../../config/config.php' => base_path('config/safeurl.php')
+		]);
+
+		 $this->mergeConfigFrom(__DIR__.'/../../config/config.php', 'safeurl'); 
 	}
 
 	/**
@@ -42,33 +46,6 @@ class SafeurlServiceProvider extends ServiceProvider {
 	public function provides()
 	{
 		return array();
-	}
-
-	/**
-	 * Register our config file
-	 *
-	 * @return void
-	 */
-	protected function registerConfig()
-	{
-		// The path to the user config file
-		$userConfigPath    = app()->configPath() . '/packages/jaybizzle/safeurl/config.php';
-
-		// Path to the default config
-		$defaultConfigPath = __DIR__ . '/../../config/config.php';
-
-		// Load the default config
-		$config = $this->app['files']->getRequire($defaultConfigPath);
-
-		if (file_exists($userConfigPath)) 
-		{       
-			// User has their own config, let's merge them properly
-			$userConfig = $this->app['files']->getRequire($userConfigPath);
-			$config     = array_replace_recursive($config, $userConfig);
-		}
-
-		// Set each of the items like ->package() previously did
-		$this->app['config']->set('safeurl::config', $config);
 	}
 
 }
