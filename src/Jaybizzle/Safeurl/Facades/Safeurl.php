58e9b43efa23407aeee6a1eb69792590eca61d65
Emirkan Acar <relakith@gmail.com>
2017-02-19 00:58:41 +0200

    Fix Errors
    Fix Errors

--- src/Jaybizzle/Safeurl/Facades/Safeurl.php
+++ src/Jaybizzle/Safeurl/Facades/Safeurl.php
@@ -1,18 +1,18 @@
-<?php
-
-namespace Jaybizzle\Safeurl\Facades;
+<?php namespace Jaybizzle\Safeurl\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Safeurl extends Facade
-{
-    /**
+class Safeurl extends Facade {
+  /**
    * Get the registered name of the component.
+   *
+   * Don't use this. Just... don't.
    *
    * @return string
    */
   protected static function getFacadeAccessor()
   {
-      return 'safeurl';
+      return 'Jaybizzle\Safeurl\Safeurl';
   }
+
 }
